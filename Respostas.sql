315091243eb257daf99cda6d8803d547f2e13c63
Aline Souza <as.alinedossantos.as@gmail.com>
2023-10-30 18:33:44 -0300

    Desafio concluído

---
+++ Respostas.sql
@@ -0,0 +1,91 @@
+-- 1
+SELECT 
+	Nome,
+	Ano
+FROM Filmes  
+
+-- 2
+SELECT *
+FROM Filmes 
+ORDER BY Ano
+
+--3
+SELECT 
+	Nome = 'Devolta para o futuro',
+	Ano = '1985',
+	Duracao = '116'
+FROM Filmes
+
+--4
+SELECT
+	Nome,
+	Ano = '1997',
+	Duracao
+FROM Filmes
+
+--5
+SELECT 
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Ano > '2000'
+
+--6
+SELECT
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao
+
+--7
+SELECT 
+	Ano,
+	COUNT(*) Quantidade
+FROM Filmes
+WHERE Ano <> ''
+GROUP BY Ano
+ORDER BY Quantidade DESC
+
+--8
+SELECT
+	*
+FROM Atores
+WHERE Genero = 'M'
+
+--9
+SELECT
+	*
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY Atores.PrimeiroNome
+
+--10
+SELECT
+	Filmes.Nome,
+	Generos.Genero
+FROM Filmes 
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON Generos.Id = FilmesGenero.IdGenero
+
+--11
+SELECT
+	Filmes.Nome,
+	Generos.Genero
+FROM Filmes 
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON Generos.Id = FilmesGenero.IdGenero
+WHERE Generos.Genero = 'Mistério'
+
+
+--12
+SELECT
+	Filmes.Nome,
+	Atores.PrimeiroNome,
+	Atores.UltimoNome,
+	ElencoFilme.Papel
+FROM Filmes
+INNER JOIN ElencoFilme ON Filmes.Id = ElencoFilme.IdFilme
+INNER JOIN Atores ON Atores.Id = ElencoFilme.IdAtor
